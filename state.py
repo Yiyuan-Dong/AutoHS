fe26cf5001a342aeb995a35bbb9f5a7ef8232fd3
Yiyuan-Dong <852427139@qq.com>
2021-06-27 14:44:55 +0800

    调整了一下文件组织形式

--- state.py
+++ state.py
@@ -1,36 +1,10 @@
 import copy
 import time
-
+from minion import Minion
 import click
 import get_screen
 import keyboard
 import sys
-
-
-class Minion:
-    def __init__(self, attack, health, is_taunt, is_divine_shield):
-        self.attack = attack
-        self.health = health
-        self.is_taunt = is_taunt
-        self.has_divine_shield = is_divine_shield
-
-    def __str__(self):
-        temp = str(self.attack) + "-" + str(self.health)
-        if self.is_taunt:
-            temp += " 嘲讽"
-        if self.has_divine_shield:
-            temp += " 圣盾"
-        return temp
-
-    def delta_h_after_damage(self, damage):
-        if self.has_divine_shield:
-            return self.attack
-        else:
-            if damage >= self.health:
-                return self.attack + self.health
-            else:
-                return damage
-
 
 class State:
     def __init__(self):
