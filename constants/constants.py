5449cfa75776be3fe1edc865df3c28331ed605f8
mysteriousdog <3076777685@qq.com>
2023-08-29 22:26:49 +0800

    update data

--- constants/constants.py
+++ constants/constants.py
@@ -1,6 +1,6 @@
 # 你的Power.log的路径, 应该在你的炉石安装目录下的`Logs/`文件夹中, 这里放的是我的路径
 # ** 一定要修改成自己电脑上的路径 **
-HEARTHSTONE_POWER_LOG_PATH = "C:\Program Files (x86)\Apps\Hearthstone\Logs\Hearthstone_2023_08_28_21_21_10\Power.log"
+HEARTHSTONE_POWER_LOG_PATH = "C:\Program Files (x86)\Apps\Hearthstone\Logs\Hearthstone_2023_08_29_21_18_48\Power.log"
 
 # 你的炉石用户名, 注意英文标点符号'#', 把后面的数字也带上
 # 可以输入中文
