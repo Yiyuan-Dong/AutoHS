54ac3de4de3e5aeea1cdf79443ffabe9d95200d5
Yiyuan Dong <852427139@qq.com>
2025-01-22 22:41:33 +0800

    星际争霸主题跟传统画风差距有点大

--- window_utils.py
+++ window_utils.py
@@ -147,7 +147,7 @@ def get_state():
 
     logger.debug(f"Similarity: main_menu: {simm_main_menu}, choose_hero: {simm_choose_hero}, matching: {simm_matching}, choose_card: {simm_choose_card}")
 
-    if simm_main_menu > 0.88:
+    if simm_main_menu > 0.83:
         return FSM_MAIN_MENU
     elif simm_choose_hero > 0.9:
         return FSM_CHOOSING_HERO
